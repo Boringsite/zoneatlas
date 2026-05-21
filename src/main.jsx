e5758d8fc2c72eebfac191eaece7c074e88ba051
Boringsite <hamilton_garth@hotmail.com>
2026-05-21 10:59:37 -0400

    Add InstallPrompt component to main render

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './WorldTimezone.jsx'
+import InstallPrompt from './InstallPrompt.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <InstallPrompt />
   </React.StrictMode>,
 )
